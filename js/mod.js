dee75bce9f2b92084aa75fab41c5454e5cab5632
dylankent26 <kentdylan26@gmail.com>
2024-06-03 16:28:45 +0100

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The Gear Tree",
+	id: "DildoDylan",
+	author: "dylankent26",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
